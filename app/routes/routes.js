22d2b8650d83b69904154271c1dacda5867677ff
gcarpi <guilherme.carpi@owinteractive.com>
2018-07-05 08:41:49 -0300

    att

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -19,26 +19,29 @@ module.exports = function(app, base64Img) {
       if (err) console.log(err);
 
       else {
-
+        console.log(filepath);
         let exec = require('child_process').exec;
 
-        let child = exec(`java -jar zxing.jar public/src/img/teste.jpg`,
+        let child = exec(`java -jar zxing.jar ${filepath}`,
 
           function(error, stdout, stderr) {
-
+            
             if (error !== null) {
 
               console.log("Error -> " + error);
 
             } else {
-
+              console.log(stdout);
+              
               let result = stdout
                 .substring(stdout.indexOf("Raw result:"), stdout.indexOf("Parsed result:"))
                 .split(':')[1]
                 .replace(/\n/g, '');
-
+              
               res.send(JSON.stringify({
+              
                 barcode: result
+              
               }));
 
             }
